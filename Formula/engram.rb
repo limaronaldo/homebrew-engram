7ba5be7347a4a8fbe84e76fec87dd3795a78fa67
Ronaldo Martins <ron@ldinho.com.br>
2026-02-13 19:04:39 -0300

    chore: update engram to v0.4.1 with all platform SHA256 hashes
    - aarch64-apple-darwin: 9e3ae08b...
    - x86_64-apple-darwin: a97cfefd...
    - aarch64-unknown-linux-gnu: 951631f5...
    - x86_64-unknown-linux-gnu: 9a67cee1...

--- Formula/engram.rb
+++ Formula/engram.rb
@@ -7,21 +7,20 @@ class Engram < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "dfc80aca209d129da90b89d2c1c2cf37bdf2390ed84e2d67a15f7ade73cd7b5d"
+      sha256 "9e3ae08bb0379d418163dfd97da513395cc973b180399bb01a9b0624ed4f2797"
     else
-      # x86_64 binary will be available once CI builds are set up
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_X86_64"
+      sha256 "a97cfefd32e6f8ddc592868981785f2f8f63a1fdc3129609833b7c2645a58e90"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "951631f5021e2a610db90b382d962ccca180f768c553db65951b5b6a5cc53e8e"
     else
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86_64"
+      sha256 "9a67cee12b27974e5100ee7f20a2e7e83ca5067d902cb0a19bf3b949ed1a1c47"
     end
   end
 
