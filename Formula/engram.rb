26aa4bef66aa5a2546b3b017788eec2d4796acc8
Ronaldo Martins <ron@ldinho.com.br>
2026-03-10 08:51:18 -0300

    chore: update engram to v0.15.0

--- Formula/engram.rb
+++ Formula/engram.rb
@@ -1,26 +1,26 @@
 class Engram < Formula
   desc "Memory for production AI agents - hybrid search, knowledge graphs, MCP protocol"
   homepage "https://github.com/limaronaldo/engram"
-  version "0.11.0"
+  version "0.15.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "c79e033efe91b8e99cacb9b2fef67e0321efa6aac9e2c6eb021d4c7212a84fe9"
+      sha256 "3d631addccab4bae1a7ce894097d1e6513e72c58a9cd406aa58888f8c0059c9f"
     else
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "d640bd6a484a9abac7f0d2364db8458cc8f578fe48ed32129177e977729cd7d1"
+      sha256 "9e2e1c6c681aaca5e1b17030b1ea340da8ab030dcd1a8c37e0c4f8d7ba8f9d5e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "df3601dbf62574020523465c0bb10c60fc2651cf9240842dd41d2b70b462a3f4"
+      sha256 "9d9fe610b83d31cc5402a37b8da112de4bb226bea76945b969b70d6906b4a2e6"
     else
       url "https://github.com/limaronaldo/engram/releases/download/v#{version}/engram-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c9d04ad62a6867603a1abd890e10c1f41841c3bef13d2fbe6cfb98dd1a0bfd8b"
+      sha256 "fa95aaefed00cadf3980a4032ac205f5befa5bead7233171340651ca57187c08"
     end
   end
 
